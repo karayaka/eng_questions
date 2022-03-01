e0ee0d8c6d2057da85fbc725e33568fcd221400f
çağrı karayakac <34353943+karayaka@users.noreply.github.com>
2022-03-01 21:44:16 +0300

    Update tools.dart

--- lib/app_tools/tools.dart
+++ lib/app_tools/tools.dart
@@ -1,5 +1,5 @@
 class Tools {
   static const String baseUrl = "https://cagnaz.com/service";
   static const String appToken =
-      "a9d3a5f0-9a50-4448-8078-244427605730-/-afd89486-9743-474f-a892-8573ac21d9bb";
+      "<ApiKey>";
 }
